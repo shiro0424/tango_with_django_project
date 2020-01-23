48f1516d5cdb0c9932cd7e1f3fe10ffc3775238a
Tiancheng Dong <2471036d@student.gla.ac.uk>
2020-01-23 00:30:26 +0000

    third commit

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!"
-		+ "</br><a href='/rango/about/'>About</a>")
+	# return HttpResponse("Rango says hey there partner!"
+	# 	+ "</br><a href='/rango/about/'>About</a>")
+	# Construct a dictionary to pass to themplate engine as its context
+	# Note the key boldmessage matches to {{ boldmessage }} in the template!
+	context_dict = {'boldmessage': 'crunchy, creamy, cookie, candy, cupcake!'}
+
+	# Return a rendered response to send to the client.
+	# We make use of the shortcut function to make our lives easier.
+	# Note that the first parameter is the template we wish to use.
+	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
 	return HttpResponse("Rango says here is the about page."
